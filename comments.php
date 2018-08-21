e63899413fb3f0f7de0f4aabf5ec1544d452a9dc
Tôi Là Tùng <toilatung@icloud.com>
2018-08-21 22:02:15 +0700

    2.0.3
    2.0.3

--- comments.php
+++ comments.php
@@ -15,60 +15,60 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
-	return;
+if (post_password_required()) {
+    return;
 }
 ?>
 
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
+// You can start editing here -- including this comment!
+if (have_comments()): ?>
 		<h2 class="title is-4">
 			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'Có một cuộc thảo luận ở &ldquo;%1$s&rdquo;', 'conggiao' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s cuộc thảo luận ở &ldquo;%2$s&rdquo;', '%1$s cuộc thảo luận ở &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'conggiao' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
+$comment_count = get_comments_number();
+if (1 === $comment_count) {
+    printf(
+        /* translators: 1: title. */
+        esc_html_e('Có một cuộc thảo luận ở &ldquo;%1$s&rdquo;', 'conggiao'),
+        '<span>' . get_the_title() . '</span>'
+    );
+} else {
+    printf( // WPCS: XSS OK.
+        /* translators: 1: comment count number, 2: title. */
+        esc_html(_nx('%1$s cuộc thảo luận ở &ldquo;%2$s&rdquo;', '%1$s cuộc thảo luận ở &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'conggiao')),
+        number_format_i18n($comment_count),
+        '<span>' . get_the_title() . '</span>'
+    );
+}
+?>
 		</h2><!-- .comments-title -->
+		<hr>
 
-		<?php the_comments_navigation(); ?>
+		<?php the_comments_navigation();?>
 
 		<?php
-			wp_list_comments( array(
-				'walker' => new comment_walker()
-			) );
-		?><!-- .comment-list -->
+wp_list_comments(array(
+    'walker' => new comment_walker(),
+));
+?><!-- .comment-list -->
 
 		<?php the_comments_navigation();
 
+endif; // Check for have_comments().
 
-	endif; // Check for have_comments().
-	
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() ) : ?>
-		<p class="no-comments"><?php esc_html_e( 'Chức năng bình luận đã khóa cho bài viết này.', 'conggiao' ); ?></p>
+// If comments are closed and there are comments, let's leave a little note, shall we?
+if (!comments_open()): ?>
+		<p class="no-comments"><?php esc_html_e('Chức năng bình luận đã khóa cho bài viết này.', 'conggiao');?></p>
 	<?php
-	endif;
+endif;
 
-	// Comment Form 
-	$commentFields =  array(
-		'author' => '<div class="field">
+// Comment Form
+$commentFields = array(
+    'author' => '<div class="field">
 		    <p class="control has-icons-left has-icons-right">
-		        <input class="input" id="author" name="author" type="text" placeholder="Tên Hiển Thị" value="' . esc_attr( $commenter['comment_author'] ) .'">
+		        <input class="input" id="author" name="author" type="text" placeholder="Tên Hiển Thị" value="' . esc_attr($commenter['comment_author']) . '">
 		        <span class="icon is-small is-left">
 		            <i class="fas fa-user-circle"></i>
 		        </span>
@@ -77,9 +77,9 @@ if ( post_password_required() ) {
 		        </span>
 		    </p>
 		</div>',
-		'email' => '<div class="field">
+    'email' => '<div class="field">
 		    <p class="control has-icons-left has-icons-right">
-		        <input class="input" id="email" name="email" type="email" value="'.esc_attr(  $commenter['comment_author_email'] ).'" placeholder="Địa chỉ Email (sẻ không hiển thị)">
+		        <input class="input" id="email" name="email" type="email" value="' . esc_attr($commenter['comment_author_email']) . '" placeholder="Địa chỉ Email (sẻ không hiển thị)">
 		        <span class="icon is-small is-left">
 		            <i class="far fa-envelope"></i>
 		        </span>
@@ -88,48 +88,48 @@ if ( post_password_required() ) {
 		        </span>
 		    </p>
 		</div>',
-		'url' => '<div class="field comment-form-url">
+    'url' => '<div class="field comment-form-url">
 		    <p class="control has-icons-left has-icons-right">
-		        <input class="input" id="url" name="url" type="text" value="'.esc_attr(  $commenter['comment_author_url'] ).'" placeholder="Địa chỉ Website">
+		        <input class="input" id="url" name="url" type="text" value="' . esc_attr($commenter['comment_author_url']) . '" placeholder="Địa chỉ Website">
 		        <span class="icon is-small is-left">
 		            <i class="fas fa-globe-asia"></i>
 		        </span>
 		    </p>
 		</div>',
-	);
-	$argsComments = array(
-		'id_form'           => 'commentform',
-		'class_form'      	=> 'comment-form',
-		'id_submit'         => 'submit',
-		'class_submit'      => 'submit button is-link',
-		'name_submit'       => 'submit',
-		'title_reply'       => __( 'Bình Luận' ),
-		'title_reply_to'    => __( 'Gửi Trả Lời Tới %s' ),
-		'cancel_reply_link' => __( 'Hủy Trả Lời' ),
-		'label_submit'      => __( 'Gửi Bình Luận' ),
-		'format'            => 'xhtml',
-		'comment_field' 	=>  '<div class="field">
+);
+$argsComments = array(
+    'id_form' => 'commentform',
+    'class_form' => 'comment-form',
+    'id_submit' => 'submit',
+    'class_submit' => 'submit button is-link',
+    'name_submit' => 'submit',
+    'title_reply' => __('Bình Luận'),
+    'title_reply_to' => __('Gửi Trả Lời Tới %s'),
+    'cancel_reply_link' => __('Hủy Trả Lời'),
+    'label_submit' => __('Gửi Bình Luận'),
+    'format' => 'xhtml',
+    'comment_field' => '<div class="field">
 		    <div class="control">
 		        <textarea id="comment" name="comment" class="textarea" placeholder="Nội Dung Bình Luận"></textarea>
 		    </div>
 		</div>',
-		'must_log_in' 		=> '<p class="must-log-in">' .
-		sprintf(
-		  __( 'Bạn cần <a href="%s">đăng nhập</a> để gửi bình luận.' ),
-		  wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
-		) . '</p>',
+    'must_log_in' => '<p class="must-log-in">' .
+    sprintf(
+        __('Bạn cần <a href="%s">đăng nhập</a> để gửi bình luận.'),
+        wp_login_url(apply_filters('the_permalink', get_permalink()))
+    ) . '</p>',
 
-		'logged_in_as' 		=> '<p class="logged-in-as">' .
-		sprintf(
-		__( 'Đăng nhập với tài khoản: <a href="%1$s">%2$s</a>. <a href="%3$s" title="Đăng xuất tài khoản này">Đăng Xuất?</a>' ),
-		  admin_url( 'profile.php' ),
-		  $user_identity,
-		  wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
-		) . '</p>',
-		'title_reply_before' => '<hr><h3 id="reply-title" class="title comment-reply-title">',
-		'fields' => apply_filters( 'comment_form_default_fields', $commentFields ),
-	);
-	comment_form($argsComments);
+    'logged_in_as' => '<p class="logged-in-as">' .
+    sprintf(
+        __('Đăng nhập với tài khoản: <a href="%1$s">%2$s</a>. <a href="%3$s" title="Đăng xuất tài khoản này">Đăng Xuất?</a>'),
+        admin_url('profile.php'),
+        $user_identity,
+        wp_logout_url(apply_filters('the_permalink', get_permalink()))
+    ) . '</p>',
+    'title_reply_before' => '<h3 id="reply-title" class="title comment-reply-title">',
+    'fields' => apply_filters('comment_form_default_fields', $commentFields),
+);
+comment_form($argsComments);
 ?>
 
 </div><!-- #comments -->
